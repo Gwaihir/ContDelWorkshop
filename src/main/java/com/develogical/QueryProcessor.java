c996a8e34bf0e0816b5a21ebce38af193acd3daa
wheemske <wim.heemskerk@capgemini.com>
2012-09-15 14:14:11 +0200

    Log

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,6 +3,8 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
+    	System.out.println(query);    	
+    	
         if (query.contains("SPA2012")) {
             return "SPA is a conference";
         } else if (query.contains("Eendje")) {
